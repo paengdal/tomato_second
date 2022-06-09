26dd5002607384613423357cb8ed4224a74ebb69
paengdal <paengdal1@gmail.com>
2022-06-09 23:14:23 +0900

    log남기는 법

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:tomato_record/splash_screen.dart';
+import 'package:tomato_record/utils/logger.dart';
 
 void main() {
+  logger.d('my first log');
   runApp(MyApp());
 }
 
@@ -9,23 +12,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SplashScreen(),
-    );
+    return FutureBuilder<Object>(
+        future: Future.delayed(
+          Duration(milliseconds: 3000),
+          () => 2000,
+        ),
+        builder: (context, snapshot) {
+          return AnimatedSwitcher(
+              duration: Duration(milliseconds: 300),
+              child: _splashLoadingWidget(snapshot));
+        });
+  }
+
+  StatelessWidget _splashLoadingWidget(AsyncSnapshot<Object> snapshot) {
+    if (snapshot.hasError) {
+      return Text('Error occur');
+    } else if (snapshot.hasData == true) {
+      return TomatoApp();
+    } else {
+      return SplashScreen();
+    }
   }
 }
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+class TomatoApp extends StatelessWidget {
+  const TomatoApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(
-          color: Colors.amber,
-        ),
-      ),
+    return Container(
+      color: Colors.amber,
     );
   }
 }
